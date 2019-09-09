d1f908e4fa11ca045363a83706ea5a642c5abebc
jakob <drusany.jakob@gmail.com>
2019-09-09 07:09:56 +0200

    Moved network visualisation to its own file

--- react/src/screens/Dashboard.jsx
+++ react/src/screens/Dashboard.jsx
@@ -1,13 +1,10 @@
 import React, {Component} from 'react'
 import '../styles/Dash.scss'
-
-const LayerType = {
-  CONV: 0,
-  POOL: 1,
-  FC:2,
-  INPUT:3,
-  FLATTEN:4
-}
+import {
+  getNetworks,
+  createNetworks
+} from '../Api'
+import NetworkVisualisation from '../components/NetworkVisualisation'
 
 class Dashboard extends Component {
   constructor(props){
@@ -16,21 +13,18 @@ class Dashboard extends Component {
     this.state = {
       networks:[],
     }
+
+    this.updateNetworkList = this.updateNetworkList.bind(this)
   }
 
   componentDidMount(){
-    fetch(`http://localhost:3005/getNetworks`,{
-      method: `GET`,
-      headers: {
-        'Authorization': `Bearer ${this.props.apiToken}`
-      }
-    }).then(response => response.json())
-    .then(json => {
-      if(json.err)
-        throw new Error(json.err)
+    this.updateNetworkList()
+  }
 
-      console.log(json)
-      this.setState({networks:json})
+  updateNetworkList(){
+    getNetworks(this.props.apiToken)
+    .then(networks => {
+      this.setState({networks:networks})
     })
     .catch(this.props.onError)
   }
@@ -52,39 +46,34 @@ class Dashboard extends Component {
     .catch(this.props.onError)
   }
 
+  updateFilters(){
+    //TODO add update filters
+  }
+
   render(){
     return (
       <div>
         <div className="rightRest">
           {
             this.state.networkData && <div>
-              <div className="visualization">
-                {
-                  this.state.networkData.shape.map((el, lI) => {
-                    return <div style={{display:`inline-block`, padding:`10px`, verticalAlign:`middle`}}>
-                    {(() => {
-                      if(el.type == LayerType.INPUT){
-                        return Array(el.d).fill(0).map((_,dI) => // depth index
-                          <div key={lI} style={{background:`white`, width:el.w, height:el.h, margin:`2px`}}></div>
-                        )
-                      }else if(el.type == LayerType.CONV || el.type == LayerType.POOl){
-                        return Array(el.d).fill(0).map((_,dI) => // depth index
-                          <div>
-                            <div key={lI} style={{background:`white`, width:el.f, height:el.f, margin:`2px`, display:`inline-block`, verticalAlign:`middle`}}></div>
-                            <div key={lI} style={{background:`white`, width:el.w, height:el.h, margin:`2px`, display:`inline-block`, verticalAlign:`middle`}}></div>
-                          </div>
-                        )
-                      }
-                    })()}</div>
-                  })
-                }
-              </div>
+              <NetworkVisualisation networkData={this.state.networkData} />
             </div>
           }
         </div>
         <div className="rightSidebar headerText">
           <div className="head">
-            Your networks
+            <div className="leftText">
+              Your networks
+            </div>
+            <div className="rightText" onClick={() => {
+              createNetworks(this.props.apiToken)
+              .then(() => {
+                this.updateNetworkList()
+              })
+              .catch(this.props.onError)
+            }}>
+              new
+            </div>
           </div>
           <div className="listOfNetworks ">
             <ul>
