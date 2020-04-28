d8662a5119ed2380ada157a2d127466894a2a556
jakob <drusany.jakob@gmail.com>
2020-04-28 20:09:33 +0200

    Updates

---
+++ precache-manifest.b71e0f95ea0a2626adfccaa972ac06e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eee2378ee8ee64b1af744acf8d41fae",
+    "url": "/CNN-js-frontend/index.html"
+  },
+  {
+    "revision": "8abd7a25b76f6589dcff",
+    "url": "/CNN-js-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "96a4a989b3ceb35b3085",
+    "url": "/CNN-js-frontend/static/js/2.4e229d0d.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/CNN-js-frontend/static/js/2.4e229d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abd7a25b76f6589dcff",
+    "url": "/CNN-js-frontend/static/js/main.0da6f750.chunk.js"
+  },
+  {
+    "revision": "14acf6e2f80066c4500b",
+    "url": "/CNN-js-frontend/static/js/runtime-main.91b1ad7a.js"
+  },
+  {
+    "revision": "09950e4d21054af7374fe478e9904969",
+    "url": "/CNN-js-frontend/static/media/cnnjs-logo.09950e4d.svg"
+  },
+  {
+    "revision": "1e6df7cf2088f3cc7ad0b73b548662d1",
+    "url": "/CNN-js-frontend/static/media/gears.1e6df7cf.svg"
+  }
+]);
